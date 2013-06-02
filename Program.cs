e60667465170609600114b9e867d014f224d2b1a
Denis <param3456@gmail.com>
2013-06-02 17:58:24 +0400

    added explicit method

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ODESolver
         static double b(double lambda, double t)
         {
             //return lambda*lambda*lambda + t;
-            return 0.25;
+            return 1;
         }
 
         static double dBLambda(double lambda, double t)
@@ -29,13 +29,13 @@ namespace ODESolver
         static double a(double lambda, double t)
         {
             //return lambda*lambda + t;
-            return 2 * lambda;
+            return -1.1 * lambda;
         }
 
         static double dALambda(double lambda, double t)
         {
             //return 2*lambda;
-            return 2;
+            return -1.1;
         }
 
         static double alpha(double labmda, double t, double h, double l)
@@ -53,18 +53,29 @@ namespace ODESolver
             return 1 + l / (2.0 * h * h) * b(labmda, t) - l * ((1 / 2.0) * d2BLambda(labmda, t) - dALambda(labmda, t));
         }
 
+        static double beta(double labmda, double t, double h, double l)
+        {
+            return 1 - l / (2.0 * h * h) * b(labmda, t) +
+                   l * ((1 / 2.0) * d2BLambda(labmda, t) - dALambda(labmda, t));
+        }
+
         static bool checkTridiagonalMatrixAlgorithmStability(double A, double B, double C, double E)
         {
             return (A > 0) && (C > 0) && (B >= A + C) && (E >= 0 && E < 1);
         }
 
+        static bool checkExplicitAlgorithmStability(double b, double l, double h)
+        {
+            return (b > 0) && ((l / (h * h)) < (1 / b));
+        }
+
         static void Main(string[] args)
         {
-            double c = 0;
-            double d = 2;
+            double c = -2.5;
+            double d = 2.5;
 
-            int N = 1000;
-            int M = 10000;
+            int N = 20;
+            int M = 100;
             List<List<double>> grid = new List<List<double>>();
 
             for (int i = 0; i < M; i++)
@@ -80,9 +91,9 @@ namespace ODESolver
             }
             
             double h = (d - c) / N;
-            double l = 0.00001;
+            double l = 0.01;
             
-            //initial distribution P(x) normal
+            ////initial distribution P(x) normal
             //double mx = (c + d) / 2;
             //double sigma = (mx - c) / 4;
 
@@ -93,7 +104,7 @@ namespace ODESolver
             //}
 
             //initial delta distibution
-            double initialX = 1;
+            double initialX = 0.5;
             int n = 145;
             for (int i = 1; i < grid[0].Count - 1; i++)
             {
@@ -102,17 +113,60 @@ namespace ODESolver
             }
 
             //normalization
-            //double sum = 0;
-            //for (int i = 0; i < grid[0].Count; i++)
-            //{
-            //    sum += grid[0][i];
-            //}
+            double sum = 0;
+            for (int i = 0; i < grid[0].Count; i++)
+            {
+                sum += grid[0][i];
+            }
 
-            //for (int i = 0; i < grid[0].Count; i++)
-            //{
-            //    grid[0][i] /= sum;
-            //}
+            for (int i = 0; i < grid[0].Count; i++)
+            {
+                grid[0][i] /= sum;
+            }
 
+            ExplicitMethod(N, M, c, h, l, grid);
+            
+            //ImplicitMethod(N, M, c, h, l, grid);
+
+            using (var writer = new StreamWriter("output.csv"))
+            {
+                foreach (var line in grid)
+                {
+                    foreach (var point in line)
+                    {
+                        writer.Write(point + ";");
+                    }
+                    writer.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+
+        private static void ExplicitMethod(int N, int M, double c, double h, double l, List<List<double>> grid)
+        {
+            for (int j = 0; j < M - 1; j++)
+            {
+                //слой сетки
+                double t = j * l;
+
+                for (int i = 1; i < N - 1; i++)
+                {
+                    double lambda = c + i * h;
+
+                    if (!checkExplicitAlgorithmStability(b(lambda, t), l, h))
+                    {
+                        Console.WriteLine("Boroda na shage: j: " + j.ToString() + " i: " + i.ToString());
+                    }
+                    grid[j + 1][i] = alpha(lambda, t, h, l) * grid[j][i + 1] + beta(lambda, t, h, l) * grid[j][i] +
+                                     gamma(lambda, t, h, l) * grid[j][i - 1];
+                }
+            }
+        }
+
+        private static void ImplicitMethod(int N, int M, double c, double h, double l, List<List<double>> grid)
+        {
             List<double> F = new List<double>(N);
             List<double> E = new List<double>(N);
 
@@ -122,14 +176,25 @@ namespace ODESolver
                 F.Add(0);
             }
 
+            //boundary conditions
+            double chi1 = 0;
+            double nu1 = 0;
+            double chi2 = 0;
+            double nu2 = 0;
+
             for (int j = 1; j < M; j++)
-            {   //слой сетки
+            {
+                //слой сетки
                 double t = j * l;
+
+                //boundary conditions
+                E[0] = chi1;
+                F[0] = nu1;
 
                 for (int i = 0; i < N - 1; i++)
                 {
                     double lambda = c + i * h;
-                    
+
                     double C = alpha(lambda, t, h, l);
                     double B = delta(lambda, t, h, l);
                     double A = gamma(lambda, t, h, l);
@@ -144,8 +209,8 @@ namespace ODESolver
                     }
                 }
 
-                grid[j][N - 1] = 0; // boundary conditions
-                for (int i = N - 2; i > 1; i--)
+                grid[j][N - 1] = (nu2 + chi2 * F[N - 1]) / (1 - chi2 * E[N - 1]); // boundary conditions
+                for (int i = N - 2; i >= 1; i--)
                 {
                     grid[j][i] = E[i + 1] * grid[j][i + 1] + F[i + 1];
                 }
@@ -166,23 +231,7 @@ namespace ODESolver
                     E[i] = 0;
                     F[i] = 0;
                 }
-                
             }
-
-            using (var writer = new StreamWriter("output.csv"))
-            {
-                foreach (var line in grid)
-                {
-                    foreach (var point in line)
-                    {
-                        writer.Write(point + ";");
-                    }
-                    writer.WriteLine();
-                }
-            }
-
-            Console.WriteLine("Finished");
-            Console.ReadLine();
         }
     }
 }
